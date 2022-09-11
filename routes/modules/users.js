4b15e8e8ea681516519a8bcd2220491232b4d49c
parker3216 <parker3216@hotmail.com>
2022-09-11 21:19:16 +0800

    feat: add message partials

---
+++ routes/modules/users.js
@@ -0,0 +1,75 @@
+const express = require('express')
+const router = express.Router()
+const bcrypt = require('bcryptjs')
+const passport = require('passport')
+
+
+const db = require('../../models')
+const Todo = db.Todo
+const User = db.User
+
+
+router.get('/login', (req, res) => {
+  res.render('login')
+})
+
+router.post('/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/users/login'
+}))
+
+router.get('/register', (req, res) => {
+  res.render('register')
+})
+
+router.post('/register', (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+  const errors = []
+  if (!name || !email || !password || !confirmPassword) {
+    errors.push({ message: '所有欄位都是必填' })
+  }
+  if (password !== confirmPassword) {
+    errors.push({ message: '密碼與驗證密碼不符！' })
+  }
+  if (errors.length) {
+    return res.render('register', {
+      errors,
+      name,
+      email,
+      password,
+      confirmPassword
+    })
+  }
+  User.findOne({ where: { email } })
+    .then(user => {
+      if (user) {
+        errors.push({ message: '此email 已經註冊過了' })
+        return res.render('register', {
+          errors,
+          name,
+          email,
+          password,
+          confirmPassword
+        })
+      }
+      return bcrypt
+        .genSalt(10)
+        .then(salt => bcrypt.hash(password, salt))
+        .then(hash => User.create({
+          name,
+          email,
+          password: hash
+        }))
+        .then(() => res.redirect('/users/login'))
+        .catch(err => console.log(err))
+    })
+    .catch(err => console.log(err))
+})
+
+router.get('/logout', (req, res) => {
+  req.logout()
+  req.flash('success_msg', '您已成功登出')
+  res.redirect('/users/login')
+})
+
+module.exports = router
