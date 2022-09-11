48269b0d177ff596ac4526af685924aa5919976f
parker3216 <parker3216@hotmail.com>
2022-09-11 21:06:39 +0800

    feat: edit router

---
+++ routes/modules/home.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const router = express.Router()
+
+
+const db = require('../../models')
+const Todo = db.Todo
+
+router.get('/', (req, res) => {
+  const UserId = req.user.id
+  return Todo.findAll({ where: { UserId }, raw: true, nest: true })
+    .then((todos) => { return res.render('index', { todos: todos }) })
+    .catch((error) => { return res.status(422).json(error) })
+})
+
+
+module.exports = router
